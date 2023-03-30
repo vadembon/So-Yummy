0cce429d1f7aa1e40f9b2a339d430757e144d533
Daria Shenkevych <daria.shenkevych@gmail.com>
2023-03-31 01:31:47 +0300

    colors explanation

--- src/constants/theme.js
+++ src/constants/theme.js
@@ -14,28 +14,38 @@ export const theme = Object.freeze({
     black: 900,
   },
   colors: {
-    primaryDarkText: '#23262A',
-    secondaryDarkText: '#3E4462',
-    tertiaryDarkText: '#001833',
+    primaryDarkText: '#23262A', /// long text of recipy
+    secondaryDarkText: '#3E4462', /// titles of recipy, ingredients
+    tertiaryDarkText: '#001833', /// title of pages (Favorites, My receipies, categories, search, shopping list
     quaternaryDarkText: '#22252A',
 
     blackText: '#000000',
 
-    primaryLightText: '#FAFAFA',
+    primaryLightText: '#FAFAFA', // in footer,  and in darkTheme
 
     whiteText: '#ffffff',
 
-    normalDark: '#1E1F28',
-    normalGrey: '#D9D9D9',
-    darkAccent: '#22252A',
-    greenAccent: '#8BAA36',
+    normalDark: '#1E1F28', // buttons
+    normalGrey: '#D9D9D9', // buttons
+    darkAccent: '#22252A', // footer
+    greenAccent: '#8BAA36', // buttons  , footer in darkTheme
 
-    greenValidation: '#3CBC81',
+    greenValidation: '#3CBC81', // in forms
     yellowValidation: '#F6C23E',
     redValidation: '#E74A3B',
 
     lightBackground: '#fafafa',
     primaryDarkBackground: '#1E1F28',
     secondaryDarkBackground: '#22252A',
+  },
+  radii: {
+    none: '0',
+    s: '4px',
+    m: '6px',
+    l: '8px',
+    xl: '10px',
+    xxl: '12px',
+    xxxl: '16px',
+    round: '50%',
   },
 });
