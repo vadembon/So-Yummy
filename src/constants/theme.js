54b07cac6571f5b88a87d0eac6ac73476c4f86c8
Daria Shenkevych <daria.shenkevych@gmail.com>
2023-03-31 02:07:24 +0300

    Update theme.js

--- src/constants/theme.js
+++ src/constants/theme.js
@@ -52,6 +52,6 @@ export const theme = Object.freeze({
     checkbox: '2px solid rgba(126, 126, 126, 0.5)',
     searchBar: ' 1px solid #F0F0F0',
     emailSubscription: '1px solid #FAFAFA',
-    userProfile : ' 1px solid #23262A',
-
+    userProfile: '1px solid #23262A'
+  }
 });
