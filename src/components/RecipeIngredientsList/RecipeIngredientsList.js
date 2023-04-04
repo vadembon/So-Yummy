e140c8f4d0c02929bc9c4ae93a51c7dce4f8c30f
Andry1343 <ua.gavrilets@gmail.com>
2023-04-05 02:50:14 +0300

    RecipePage_Update_02

--- src/components/RecipeIngredientsList/RecipeIngredientsList.js
+++ src/components/RecipeIngredientsList/RecipeIngredientsList.js
@@ -1,12 +1,23 @@
 import {
   AddToList,
-  Container,
   Ingredients,
   IngredientsList,
   Number,
+  IngedientsListUl,
+  IngedientsItemLi,
+  IngedientsImg,
+  Wrap,
+  IngedientsTitle,
+  IngedientsMeasure,
+  IngedientsCheck,
+  CheckBoxWrap,
+  CheckBoxLabel,
+  CheckMarkIcon,
 } from './RecipeIngredientsList.styled';
 
-export const RecipeIngredientsList = () => {
+import { Container } from 'commonComponents/Container';
+
+export const RecipeIngredientsList = ({ ingredients }) => {
   return (
     <Container>
       <IngredientsList>
@@ -14,6 +25,29 @@ export const RecipeIngredientsList = () => {
         <Number>Number</Number>
         <AddToList>Add to list</AddToList>
       </IngredientsList>
+
+      <IngedientsListUl>
+        {ingredients &&
+          ingredients.map(({ image, _id, measure, name }) => {
+            return (
+              <IngedientsItemLi key={_id}>
+                <Wrap>
+                  {<IngedientsImg src={image ? image : ''} alt="Ingredient" />}
+                  <IngedientsTitle>{name}</IngedientsTitle>
+                </Wrap>
+                <Wrap>
+                  <IngedientsMeasure>{measure}</IngedientsMeasure>
+                  <CheckBoxLabel htmlFor={name}>
+                    <IngedientsCheck type="checkbox" id={name} />
+                    <CheckBoxWrap>
+                      <CheckMarkIcon />
+                    </CheckBoxWrap>
+                  </CheckBoxLabel>
+                </Wrap>
+              </IngedientsItemLi>
+            );
+          })}
+      </IngedientsListUl>
     </Container>
   );
 };
