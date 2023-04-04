ca4392d8d3ae88a5752d17db6b02f339d69bef0c
Ekaterina Silchenko <maksil.17@gmail.com>
2023-04-04 12:19:55 +0300

    add commit
    serch form, select, mainPageTitle

--- src/components/SearchBar/SearchBar.styled.js
+++ src/components/SearchBar/SearchBar.styled.js
@@ -1,1 +1,6 @@
 import styled from 'styled-components';
+export const WrapperSearchBar = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+`;
---
+++ src/pages/SearchPage/SearchPage.styled.js
@@ -0,0 +1,5 @@
+import styled from 'styled-components';
+
+export const WrapperSerchBar = styled.div`
+  display: block;
+`;
