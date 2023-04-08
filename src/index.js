39061eff94ad6b503b50bacb2b3c4b757d3877cd
Marina Sereda <marina.gorb@gmail.com>
2023-04-08 15:05:57 +0300

    feat: save token to localStorage, add PageLoader

--- src/index.js
+++ src/index.js
@@ -6,10 +6,11 @@ import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 import { store } from 'redux/store';
 import { Provider } from 'react-redux';
+import { PersistGate } from 'redux-persist/integration/react';
 import { Toaster } from 'react-hot-toast';
 import { App } from 'components/App';
 import { theme } from './constants';
-
+import { persistor } from 'redux/store';
 import './index.css';
 
 const queryClient = new QueryClient({
@@ -23,15 +24,17 @@ const queryClient = new QueryClient({
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <Provider store={store}>
-      <QueryClientProvider client={queryClient}>
-        <ThemeProvider theme={theme}>
-          <BrowserRouter basename="/command_project_React_Node">
-            <App />
-            <Toaster position="bottom-right" />
-          </BrowserRouter>
-        </ThemeProvider>
-        <ReactQueryDevtools initialIsOpen={false} position="bottom-right" />
-      </QueryClientProvider>
+      <PersistGate loading={null} persistor={persistor}>
+        <QueryClientProvider client={queryClient}>
+          <ThemeProvider theme={theme}>
+            <BrowserRouter basename="/command_project_React_Node">
+              <App />
+              <Toaster position="bottom-right" />
+            </BrowserRouter>
+          </ThemeProvider>
+          <ReactQueryDevtools initialIsOpen={false} position="bottom-right" />
+        </QueryClientProvider>
+      </PersistGate>
     </Provider>
   </React.StrictMode>
 );
