93863cb52f998258462069223d72618f29c43b92
Daria Shenkevych <daria.shenkevych@gmail.com>
2023-04-10 18:18:46 +0300

    NotFound base styles added
    need check image  and  spinach

--- src/pages/NotFoundPage/NotFoundPage.js
+++ src/pages/NotFoundPage/NotFoundPage.js
@@ -1,3 +1,19 @@
-export const NotFoundPage = () => {
-  return <div>NotFoundPage</div>;
+import React from 'react';
+import NotFound from '../../components/NotFound/NotFound';
+import { PageBox } from 'commonComponents/PageBox';
+import { Spinach } from 'commonComponents/Spinach';
+
+const NotFoundPage = () => {
+  return (
+        <PageBox>
+        <Spinach />
+          <NotFound />
+      </PageBox>
+      
+ 
+  
+
+  );
 };
+
+export default NotFoundPage;
