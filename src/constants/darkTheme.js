1af015b328e8da30fdd5ea6e0a139e9ceaf1fa6d
Daria Shenkevych <daria.shenkevych@gmail.com>
2023-04-11 22:59:31 +0300

    dark theme final

--- src/constants/darkTheme.js
+++ src/constants/darkTheme.js
@@ -1,36 +1,55 @@
 export const lightTheme = {
-  primaryLightText: '#FAFAFA', // in footer,  and in darkTheme,
+    primaryLightText: '#FAFAFA', // in footer,  and in darkTheme,
 
-  normalDark: '#1E1F28', // buttons
-  primaryDarkText: '#23262A', /// long text of recipy? header text
-  secondaryDarkText: '#3E4462', /// titles of recipy, ingredients
-  tertiaryDarkText: '#001833', /// title of pages (Favorites, My receipies, categories, search, shopping list
-  mainPageButtons: '#8AA936', /// main page buttons except footer! also my receipy
-  greenAccent: '#8BAA36', // buttons  , footer in darkTheme     !!!!!!!!!!!!!!!!  checkbox
-  SocialIcons: '#8BAA36', //  footer ND ADD RECEIPE social icons !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
-  footerSubscribeButtonBG: '#8BAA36', //  !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
-  footerTextLight: '#FAFAFA', // in foter text  !!!!!!!!!!!!!!!!!!!
-  footerRightsReserved: '#22252A', // footer  text rights reserved
-  footerBG: '#22252A', // dark color in footer
-  headerAccentText: '#8BAA36',
-  categoriesNames: ' #BDBDBD ',
-  categoriesAccent: '#8BAA36',
-  secondaryDarkBackground: '#22252A', ///  favorites button
-  recipyLightBgItem: '  #EBF3D4',
-  myRecipyDeleteButton: '#8BAA36', // my recipe  delete button background, no change
-  normalGrey: '#f0f0f0', /// border in search form
-  //  no change
-  whiteText: '#ffffff',
-  blackText: '#000000',
-  quaternaryDarkText: '#22252A',
-  greenValidation: '#3CBC81', // in forms
-  yellowValidation: '#F6C23E',
-  redValidation: '#E74A3B',
+    normalDark: '#1E1F28', // buttons
+    primaryDarkText: '#23262A', /// long text of recipy? header text
+    secondaryDarkText: '#3E4462', /// titles of recipy, ingredients
+    tertiaryDarkText: '#001833', /// title of pages (Favorites, My receipies, categories, search, shopping list
+    mainPageButtons: '#8AA936', /// main page buttons except footer! also my receipy
+    greenAccent: '#8BAA36', // buttons  , footer in darkTheme     !!!!!!!!!!!!!!!!  checkbox
+    SocialIcons: '#8BAA36', //  footer ND ADD RECEIPE social icons !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+    footerSubscribeButtonBG: '#8BAA36', //  !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+    footerTextLight: '#FAFAFA', // in foter text  !!!!!!!!!!!!!!!!!!!
+    footerRightsReserved: '#22252A', // footer  text rights reserved
+    footerBG: '#22252A', // dark color in footer
+    headerAccentText: '#8BAA36',
+    categoriesNames: ' #BDBDBD ',
+    categoriesAccent: '#8BAA36',
+    secondaryDarkBackground: '#22252A', ///  favorites button
+    recipyLightBgItem: '  #EBF3D4',
+    myRecipyDeleteButton: '#8BAA36', // my recipe  delete button background, no change
+    normalGrey: '#f0f0f0', /// border in search form
+    //  no change
+    whiteText: '#ffffff',
+    blackText: '#000000',
+    quaternaryDarkText: '#22252A',
 
-  lightBackground: '#fafafa',
-  primaryDarkBackground: '#1E1F28',
-  messageBackground: '#53545599',
-};
+
+
+    lightBackground: '#fafafa',
+    primaryDarkBackground: '#1E1F28',
+    messageBackground: '#53545599',
+
+
+    secondaryGrey: '#656565',
+    darkAccent: '#22252A', // footer
+    lightGray: '#bdbdbd', //categories list text
+    greenValidation: '#3CBC81', // in forms
+    yellowValidation: '#F6C23E',
+    redValidation: '#E74A3B',
+  
+    secondaryAccent: '#EBF3D4',
+
+    whiteBackground: '#ffffff',
+
+  
+    secondaryLightBackground: '#ECECEC',
+    secondaryGreyBg: '#C4C4C4',
+    shoppingListProductName: '#3e4462',
+}
+
+  
+  //////////////////////////////////////////
 
 export const darkTheme = {
   primaryLightText: '#1E1F28', // black background
