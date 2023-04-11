ee994cb524cb8a47b45653992d61d5219d1952f8
Marina Sereda <marina.gorb@gmail.com>
2023-04-11 18:29:49 +0300

    feat: add prepareShoppingList function

--- src/api/helpers/prepareShoppingList.js
+++ src/api/helpers/prepareShoppingList.js
@@ -1,43 +1,47 @@
 const prepareShoppingList = () => {
   const ingredList = [
-    { ttl: 'banana', measure: '1 cup' },
-    { ttl: 'tomato', measure: '1 kg' },
-    { ttl: 'banana', measure: '1 cup' },
-    { ttl: 'milk', measure: '1 spoon' },
-    { ttl: 'banana', measure: '1 cup' },
-    { ttl: 'milk', measure: '200 ml' },
-    { ttl: 'tomato', measure: '1 kg' },
-    { ttl: 'banana', measure: '100 g' },
-    { ttl: 'milk', measure: '1 spoon' },
-    { ttl: 'banana', measure: '1 cup' },
+    { ttl: 'banana', measure: '1 cup', id: '1', recipe: '1a' },
+    { ttl: 'tomato', measure: '1 kg', id: '2', recipe: '1a' },
+    { ttl: 'banana', measure: '0.5 cup', id: '1', recipe: '2a' },
+    { ttl: 'milk', measure: '1 spoon', id: '3', recipe: '3a' },
+    { ttl: 'banana', measure: '1 cup', id: '1', recipe: '9a' },
+    { ttl: 'milk', measure: '200 ml', id: '3', recipe: '4a' },
+    { ttl: 'tomato', measure: '0.8 kg', id: '2', recipe: '6a' },
+    { ttl: 'banana', measure: '100 g', id: '1', recipe: '7a' },
+    { ttl: 'milk', measure: '0,3 spoon', id: '3', recipe: '1a' },
+    { ttl: 'banana', measure: '1 cup', id: '1', recipe: '8a' },
   ];
-  const ingredients = [];
+  const sortedIngredients = [];
+  const summaryList = [];
+  const sortedShoppingList = [];
 
   while (ingredList.length > 0) {
-    const foundIndexes = [];
-    const ingredient = ingredList.filter((item, index) => {
-      if (item.ttl === ingredList[0].ttl) {
-        // console.log('index', index);
-
-        foundIndexes.push(index);
-      }
-      return item.ttl === ingredList[0].ttl;
-    });
+    const ingredient = ingredList.filter(
+      (item, index) => item.ttl === ingredList[0].ttl
+    );
+    sortedIngredients.push(ingredient);
     //-----
-    ingredients.push(ingredient);
-    console.log('foundIndexes', foundIndexes);
-
-    for (let i = 0; i < foundIndexes.length; i++) {
-      console.log(foundIndexes[i]);
-      ingredList.splice(foundIndexes[i], 1);
-      console.log(ingredList);
-    }
-
-    // console.log('ingredList', ingredList);
+    ingredient.forEach(item =>
+      ingredList.splice(
+        ingredList.findIndex(ingr => ingr.ttl === item.ttl),
+        1
+      )
+    );
   }
+  sortedIngredients.forEach(ingr => {
+    ingr.forEach(item => {
+      item.measure = item.measure.replace(',', '.').trim();
+      item.quantity = Number.parseFloat(item.measure);
+      item.unit = item.measure.replace(item.quantity.toString(), '').trim();
+      delete item.measure;
 
-  // console.log('ingredient', ingredients);
-  // console.log('ingredList', ingredList);
+      // sortedShoppingList.push(item.filter(({ unit }) => unit === item[0].unit));
+    });
+  });
+
+  console.log('sortedIngredients', sortedIngredients);
+  console.log('ingredList', ingredList);
+  console.log('sortedShoppingList', sortedShoppingList);
 };
 
 prepareShoppingList();
