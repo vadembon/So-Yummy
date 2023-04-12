4f2b0fb375809b393afeb3763bc30b098f90871a
AlexAmanitas <104567917+AlexAmanitas@users.noreply.github.com>
2023-04-12 22:11:43 +0300

    MotivationModal

--- src/components/MotivatingModal/MotivatingModal.js
+++ src/components/MotivatingModal/MotivatingModal.js
@@ -1,3 +1,38 @@
+import { useState, useEffect, useCallback } from "react";
+import io from "socket.io-client";
+import { showMessage } from 'components/Message';
+
+const socket = io.connect("http://localhost:3005");
+
 export const MotivatingModal = () => {
-  return <div>MotivatingModal</div>;
-};
+
+  // объявляем обработчик события userRegister
+  const handleUserRegister = useCallback((data) => {
+    console.log(data)
+    showMessage(data.name)
+  }, [])
+
+  const handleFavorite = useCallback((data) => {
+    console.log(data)
+    showMessage(`You add to favorites ${data.favorite}`)
+  }, [])
+
+  // подписка на события сокета при монтировании компонента
+  useEffect(() => {
+    console.log('object')
+    socket.on("userRegister", handleUserRegister);
+    socket.on("favoriteRecipe", handleFavorite);
+   
+    // отписываемся от события при размонтировании компонента
+    return () => {
+      socket.off("userRegister", handleUserRegister);
+      socket.off("favoriteRecipe", handleFavorite);
+    };
+  }, [handleUserRegister]);
+
+  // return (
+  //   <>
+  //      </>
+  // )
+}
+
