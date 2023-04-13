0811df73b9b46acea0c2be794c45e5654e393514
Daria Shenkevych <daria.shenkevych@gmail.com>
2023-04-13 17:41:42 +0300

    update theme  from colors

--- src/constants/theme.js
+++ src/constants/theme.js
@@ -29,37 +29,57 @@ export const theme = Object.freeze({
     content: '-0.02em',
   },
   colors: {
-    primaryDarkText: '#23262A', /// long text of recipy
-    secondaryDarkText: '#3E4462', /// titles of recipy, ingredients
-    tertiaryDarkText: '#001833', /// title of pages (Favorites, My receipies, categories, search, shopping list
-    quaternaryDarkText: '#22252A',
-    footerBG: '#22252A',
-
-    blackText: '#000000',
-
-    primaryLightText: '#FAFAFA', // in footer,  and in darkTheme
-    secondaryGrey: '#656565',
-    whiteText: '#ffffff',
-
+    primaryLightText: '#FAFAFA', // in footer,  and in darkTheme,
+    primaryLightTextNoChange: '#FAFAFA', //  shoppingList
     normalDark: '#1E1F28', // buttons
-    normalGrey: '#f0f0f0', // buttons, background in add recipy, search
-    darkAccent: '#22252A', // footer
-    greenAccent: '#8BAA36', // buttons  , footer in darkTheme
-    lightGray: '#bdbdbd', //categories list text
+    primaryDarkText: '#23262A', /// long text of recipy? header text
+    primaryDarkTextNoChange: '#23262A', /// long text of recipy? header text
+    secondaryDarkText: '#3E4462', /// titles of recipy, ingredients
+    secondaryDarkTextNoChange: '#3E4462', ///
+    tertiaryDarkText: '#001833', /// title of pages (Favorites, My receipies, categories, search, shopping list
+    mainPageButtons: '#8AA936', /// main page buttons except footer! also my receipy
+    greenAccent: '#8BAA36', // buttons  , footer in darkTheme     !!!!!!!!!!!!!!!!  checkbox
+    greenAccentSearch: '#8BAA36', // search buttons
+
+    SocialIcons: '#8BAA36', //  footer ND ADD RECEIPE social icons !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+    footerSubscribeButtonBG: '#8BAA36', //  !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+    footerTextLight: '#FAFAFA', // in foter text  !!!!!!!!!!!!!!!!!!!
+    footerRightsReserved: '#22252A', // footer  text rights reserved
+    footerBG: '#22252A', // dark color in footer
+    headerAccentText: '#8BAA36',
+    categoriesNames: ' #BDBDBD ',
+    categoriesAccent: '#8BAA36',
+    secondaryDarkBackground: '#22252A', ///  favorites button
+    recipyLightBgItem: '  #EBF3D4',
+    myRecipyDeleteButton: '#8BAA36', // my recipe  delete button background, no change
+    normalGrey: '#f0f0f0', /// border in search form
+    normalGreySelect: '#f0f0f0', /// background-color in search select
+    lightSelector: '#FAFAFA', // background-color in search select option
+    whiteBackgroundToBlack: '#ffffff', /// favorites bg
+    blackCloseIconToWhite: '#333333', // icon close at shopping list
+    blackTextToWhite: '#000000',
+    //  no change
+    whiteText: '#ffffff',
+    blackText: '#000000',
+    quaternaryDarkText: '#22252A',
 
     greenValidation: '#3CBC81', // in forms
     yellowValidation: '#F6C23E',
     redValidation: '#E74A3B',
-    secondaryAccent: '#EBF3D4',
 
     lightBackground: '#fafafa',
-    whiteBackground: '#ffffff',
+    lightBackgroundBody: '#fafafa',
     primaryDarkBackground: '#1E1F28',
-    secondaryDarkBackground: '#22252A',
+    messageBackground: '#53545599',
 
-    secondaryLightBackground: '#ECECEC',
+    secondaryGrey: '#656565',
     secondaryGreyBg: '#C4C4C4',
-
+    darkAccent: '#22252A', // footer
+    lightGray: '#bdbdbd', //categories list text
+    secondaryAccent: '#EBF3D4',
+    whiteBackground: '#ffffff',
+    whiteBackgroundDish: '#ffffff',
+    secondaryLightBackground: '#ECECEC',
     shoppingListProductName: '#3e4462',
   },
   radii: {
