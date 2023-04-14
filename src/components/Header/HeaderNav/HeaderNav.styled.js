928076f94ef2f03cd121d2d1ec2db4a2674cbeff
Dariia Shenkevych <65854631+DariaTufar@users.noreply.github.com>
2023-04-14 18:30:43 +0300

    Revert "header fix"

--- src/components/Header/HeaderNav/HeaderNav.styled.js
+++ src/components/Header/HeaderNav/HeaderNav.styled.js
@@ -2,15 +2,15 @@ import styled from "styled-components";
 import { NavLink } from "react-router-dom";
 
 export const Navigation = styled.nav`
-  display: flex;
-  align-items: center;
-  justify-content: center;
-  flex-direction: column;
-  gap: 32px;
-  @media screen and (min-width: 768px) {
-    gap: 40px;
-  }
-  @media screen and (min-width: 1440px) {
+display: flex; 
+align-items: center; 
+justify-content: center;
+flex-direction: column;
+gap: 32px;
+   @media screen and (min-width: 768px) {
+     gap: 40px;
+   }
+   @media screen and (min-width: 1440px) {
     flex-direction: row;
     gap: 30px;
     justify-content: space-between;
@@ -19,36 +19,28 @@ export const Navigation = styled.nav`
   }
 `;
 
-export const LocationList = styled.div`
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-  justify-content: center;
-  gap: 32px;
-`;
-
 export const LocationLink = styled(NavLink)`
   display: flex;
   gap: 8px;
   justify-content: center;
   align-items: center;
   text-decoration: none;
-  font-family: ${(p) => p.theme.fonts.primary};
-  font-weight: ${(p) => p.theme.fontWeights.medium};
-  letter-spacing: ${(p) => p.theme.letterSpacings.content};
-  line-height: ${(p) => p.theme.lineHeights.subheader};
-  transition: ${(p) => p.theme.transitions.main};
+  font-family: ${p => p.theme.fonts.primary};
+  font-weight: ${p => p.theme.fontWeights.medium};
+  letter-spacing: ${p => p.theme.letterSpacings.content};
+  line-height: ${p => p.theme.lineHeights.subheader};
+  transition: ${p => p.theme.transitions.main};
   font-size: 18px;
-
+  
   :hover,
   :focus-visible {
-    color: ${(p) => p.theme.colors.greenAccent};
-  }
-  &:hover svg,
-  &:focus svg {
-    stroke: ${(p) => p.theme.colors.greenAccent};
-  }
-  @media screen and (min-width: 1440px) {
+     color: ${p => p.theme.colors.greenAccent};
+   }
+   &:hover svg,
+   &:focus svg {
+    stroke: ${p => p.theme.colors.greenAccent};
+   }
+   @media screen and (min-width: 1440px) {
     font-size: 24px;
   }
   @media screen and (min-width: 1440px) {
@@ -69,3 +61,6 @@ export const LocationLink = styled(NavLink)`
     }
   }
 `;
+
+
+
